e4b005fb79d06ab27db0ce596290c8638a1bb0e6
TimurFyatkhullin <rkadlwhy@gmail.com>
2026-06-23 20:22:29 +0300

    add ExternalID and IsTeacher to UserInfo

--- go-sdk/user_types.go
+++ go-sdk/user_types.go
@@ -1,8 +1,9 @@
 package wasmplugin
 
 // UserInfo contains basic information about a user.
-// The struct is intentionally minimal and will grow over time.
 type UserInfo struct {
-	ID       int64  `msgpack:"id" json:"id"`
-	FullName string `msgpack:"full_name,omitempty" json:"full_name,omitempty"`
+	ID         int64  `msgpack:"id" json:"id"`
+	FullName   string `msgpack:"full_name,omitempty" json:"full_name,omitempty"`
+	ExternalID string `msgpack:"external_id,omitempty" json:"external_id,omitempty"`
+	IsTeacher  bool   `msgpack:"is_teacher,omitempty" json:"is_teacher,omitempty"`
 }
